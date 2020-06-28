42e2d5a8b3be3732a9503077b705f8753f1f7de1
Sergey Orlov <54174889+orlovse@users.noreply.github.com>
2020-06-28 11:11:11 +0300

    Update index.js

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,55 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
-
+export function sortStrings(arr, param = 'asc') {
+  const newArr = [...arr];
+  //   let collator = new Intl.Collator();
+  //   if (param === 'asc') {
+  //     return newArr.sort((a, b) => collator.compare(a, b));
+  //   }
+  //   if (param === 'desc') {
+  //     return newArr.sort((a, b) => collator.compare(b, a));
+  //   }
+  if (param === 'asc') {
+    return newArr.sort((a, b) => {
+      let newA = a.toLowerCase();
+      let newB = b.toLowerCase();
+      if (newA[0] === 'ё') {
+        newA = 'е';
+      }
+      if (newB[0] === 'ё') {
+        newB = 'е';
+      }
+      if (newA > newB) {
+        return 1;
+      }
+      if (newA < newB) {
+        return -1;
+      }
+      if (newA === newB) {
+        return -1;
+      }
+    });
+  }
+  if (param === 'desc') {
+    return newArr.sort((a, b) => {
+      let newA = a.toLowerCase();
+      let newB = b.toLowerCase();
+      if (newA[0] === 'ё') {
+        newA = 'е';
+      }
+      if (newB[0] === 'ё') {
+        newB = 'е';
+      }
+      if (newA > newB) {
+        return -1;
+      }
+      if (newA < newB) {
+        return 1;
+      }
+      if (newA === newB) {
+        return -1;
+      }
+    });
+  }
 }
